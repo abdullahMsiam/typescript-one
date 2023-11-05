9d7579b401e652a1256db6b84e02508d8ab4ba61
Abdullah Muhammad Siam <abdullahms838@gmail.com>
2023-11-05 15:25:25 +0600

    object destructuring

---
+++ module1/src/18.ts
@@ -0,0 +1,20 @@
+{
+
+    const user = {
+        id: 123421,
+        name: {
+            fName: "abdul",
+            lName: "ali"
+        },
+        address: "Dhaka"
+    }
+
+    const {
+        id,
+        name: { fName: firstName },
+    } = user;
+
+
+
+
+}
