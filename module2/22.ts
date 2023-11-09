2c2821166b0e39240fd36b433346cff0f6b63289
Abdullah Muhammad Siam <abdullahms838@gmail.com>
2023-11-09 16:08:23 +0600

    interface apply in array and function --> 22.ts

--- module2/22.ts
+++ module2/22.ts
@@ -6,7 +6,21 @@
         name: string,
         roll: number
     }
+    /*------- Interface--- */
+    interface user2 {
+        name: string,
+        roll: number
+    }
+    // extend
     type userAndRole = User1 & { role: string };
+
+    interface userAndRole2 extends user2 {
+        role: string
+    }
+    interface userAndRole2 extends user1 {
+        role: string
+    }
+
     const user1: User1 = {
         name: "abdullah",
         roll: 1212,
@@ -17,11 +31,26 @@
         role: "almighty"
     }
 
-    /*------- Interface--- */
-    interface user2 {
-        name: string,
-        roll: number
+
+
+    /* array with type alias interface */
+
+    type Roll1 = number[];
+    const roll1: Roll1 = [1, 2, 3];
+
+    interface Roll2 {
+        [index: number]: number;
     }
+    const Roll2: Roll2 = [3, 4, 5]
+
+
+    type Add = (n1: number, n2: number) => number;
+    interface Add2 {
+        (n1: number, n2: number): number;
+    }
+
+    const add: Add2 = (num1, num2) => num1 + num2;
+
 
 
 
