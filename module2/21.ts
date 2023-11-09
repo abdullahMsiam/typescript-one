d6c6e2007e7d8a2755201d094045b7fbd6a9f4b9
Abdullah Muhammad Siam <abdullahms838@gmail.com>
2023-11-09 12:10:40 +0600

    Assertion tsc ---> 21

---
+++ module2/21.ts
@@ -0,0 +1,41 @@
+{
+
+    // -----------ASSERTION ----------------
+
+    let anything: any;
+    anything = 123;
+    (anything as number);
+
+
+    const kgtogm = (value: number | string): number | string | undefined => {
+        if (typeof value === "number") {
+            return value * 1000;
+        }
+        else if (typeof value === "string") {
+            return parseFloat(value) * 1000;
+        }
+    }
+
+    const res1 = kgtogm("1000") as string;
+    const res2 = kgtogm(10) as number;
+
+    console.log(res1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
